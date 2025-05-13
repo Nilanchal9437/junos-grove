d9a014461cbc3422d1cee095eecfa1b01a54dbcc
Nilanchal9437 <nilanchal.patra3777@gmail.com>
2025-05-13 14:57:10 +0530

    bug fixes

--- src/feature/home/MainContent.tsx
+++ src/feature/home/MainContent.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 import { GiJungle, GiTreasureMap } from "react-icons/gi";
-import { NavButton } from '@/feature/home/NavigationButtons';
+import { NavButton } from "@/feature/home/NavigationButtons";
 
 const MainContent: React.FC = () => {
   return (
@@ -18,9 +18,7 @@ const MainContent: React.FC = () => {
         transition={{ duration: 0.8, delay: 0.2 }}
         className="mb-20"
       >
-        <h1 className="text-6xl md:text-8xl font-bold text-white mb-6">
-          Juno
-        </h1>
+        <h1 className="text-6xl md:text-8xl font-bold text-white mb-6">Juno</h1>
         <h2 className="text-4xl md:text-6xl font-semibold text-white mb-4">
           Crypto Jungle
         </h2>
@@ -34,35 +32,35 @@ const MainContent: React.FC = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8, delay: 0.4 }}
       >
-        <Link href="/begin">
-          <motion.button
-            className="px-6 py-2 text-lg font-medium text-white 
+        {/* <Link href="/begin"> */}
+        <motion.button
+          className="px-6 py-2 text-lg font-medium text-white 
             bg-green-800/20 hover:bg-green-700/30 
             border-2 border-green-500 rounded-[30px]
             transition-all duration-300 
             shadow-[0_0_15px_rgba(34,197,94,0.2)] 
             hover:shadow-[0_0_25px_rgba(34,197,94,0.4)] pointer-events-auto"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            Begin now
-          </motion.button>
-        </Link>
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          Begin now
+        </motion.button>
+        {/* </Link> */}
       </motion.div>
-        <NavButton
-          href="/junos-grove"
-          text="Juno's Grove"
-          icon={<GiJungle className="w-5 h-5" />}
-          className="pointer-events-auto absolute top-[60%] right-16"
-        /> 
-         <NavButton
-          href="/treasure-map"
-          text="Treasure Map"
-          icon={<GiTreasureMap className="w-6 h-6" />}
-          className="pointer-events-auto transform absolute top-[90%] left-[5%]"
-        />
+      <NavButton
+        href="/junos-grove"
+        text="Juno's Grove"
+        icon={<GiJungle className="w-5 h-5" />}
+        className="pointer-events-auto absolute top-[60%] right-16"
+      />
+      <NavButton
+        href="/treasure-map"
+        text="Treasure Map"
+        icon={<GiTreasureMap className="w-6 h-6" />}
+        className="pointer-events-auto transform absolute top-[90%] left-[5%]"
+      />
     </motion.div>
   );
 };
 
-export default MainContent; 
+export default MainContent;
